c3ea802be596ea2289b29957d8e7722f1dff0855
dwiah <dwiahmaddzulhijjah@gmail.com>
2021-08-09 14:05:22 +0700

    Pertama

---
+++ hello.R
@@ -0,0 +1,12 @@
+teks1 = "Selamat datang di dunia R teman2"
+teks2 = "Anda akan terkesima dengan kehebatan saya, eh mksdnya R"
+
+teks1
+
+
+list1 = list("Ayam","Kambing","Sapi")
+list1
+
+3 ^ 3
+
+
